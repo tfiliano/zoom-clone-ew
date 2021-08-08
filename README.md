9fd5f7c6b17b3a96eb2715f4664763160407eb3c
Thiago Rosa <thiago@innovatelive.tv>
2021-08-08 17:52:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# zoom-clone-ew
+
+Learning and practicing new knowledge acquired with Erick Wendel in his course JS Experts and his JS week`s.
+
+In this project we could see some nice things:
+ - Builder pattern 
+ - Separation of responsabilities
+ - mediaDevices API
+ - mediaRecorder API
+ - A little of webRTC - the most part of if was handled by the PeerJS
+ - Socket.IO
+
+I could highlight the cool things that can facility our day-a-day: Map(), use OO for create some classes even without typoscript, nice way to create closure functions to apply this in our busisness class.
---
+++ public/pages/room/src/util/peer.js
@@ -0,0 +1,78 @@
+class PeerBuilder {
+  constructor({ peerConfig }) {
+    this.peerConfig = peerConfig;
+
+    const defaultFunctionValue = () => {};
+    this.onError = defaultFunctionValue;
+    this.onCallReceived = defaultFunctionValue;
+    this.onConnectionOpened = defaultFunctionValue;
+    this.onPeerStreamReceived = defaultFunctionValue;
+    this.onCallError = defaultFunctionValue;
+    this.onCallClose = defaultFunctionValue;
+  }
+
+  setOnCallError(fn) {
+    this.onCallError = fn;
+    return this;
+  }
+  setOnCallClose(fn) {
+    this.onCallClose = fn;
+    return this;
+  }
+  setOnError(fn) {
+    this.onError = fn;
+    return this;
+  }
+  setOnCallReceived(fn) {
+    this.onCallReceived = fn;
+    return this;
+  }
+  setOnConnectionOpened(fn) {
+    this.onConnectionOpened = fn;
+    return this;
+  }
+  setOnPeerStreamReceived(fn) {
+    this.onPeerStreamReceived = fn;
+    return this;
+  }
+
+  _prepareCallEvent(call) {
+    call.on("stream", (stream) => this.onPeerStreamReceived(call, stream));
+    call.on("error", (stream) => this.onCallError(call, stream));
+    call.on("close", (_) => this.onCallClose(call));
+    this.onCallReceived(call);
+  }
+
+  //add event behavior for who call us
+  _preparePeerInstanceFunction(peerModule) {
+    class PeerCustomModule extends peerModule {}
+    const peerCall = PeerCustomModule.prototype.call;
+    const context = this;
+
+    PeerCustomModule.prototype.call = function (id, stream) {
+      const call = peerCall.apply(this, [id, stream]);
+      //we get the call and we set the same events for the call I am doing
+      context._prepareCallEvent(call);
+
+      return call;
+    };
+
+    return PeerCustomModule;
+  }
+
+  build() {
+    // const peer = new Peer(...this.peerConfig);
+    const PeerCustomInstance = this._preparePeerInstanceFunction(Peer);
+    const peer = new PeerCustomInstance(...this.peerConfig);
+
+    peer.on("error", this.onError);
+    peer.on("call", this._prepareCallEvent.bind(this));
+
+    return new Promise((resolve) =>
+      peer.on("open", (id) => {
+        this.onConnectionOpened(peer);
+        return resolve(peer);
+      })
+    );
+  }
+}
